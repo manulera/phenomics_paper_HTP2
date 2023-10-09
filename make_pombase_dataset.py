707bc9b0b460c063a267942408a1a7c5f145d52e
Manuel Lera Ramirez <manulera14@gmail.com>
2023-10-09 11:00:56 +0100

    removed invalid ids, or substituted by synonyms

--- make_pombase_dataset.py
+++ make_pombase_dataset.py
@@ -18,6 +18,22 @@ print('the following conditions will be dropped (see https://github.com/pombase/
 data = data[~pandas.isna(data['FYPO ID'])]
 data['Condition'] = data['fyeco_terms']
 
+# Here we substitute certain systematic IDs by synonyms, and others we dropped, based on the file
+# data/systematic_id_dict.tsv
+# Previously, I checked that none of the synonyms used already existed in the dataset
+
+systematic_id_dict = pandas.read_csv('data/systematic_id_dict.tsv', sep='\t', names=['systematic_id', 'synonym'], na_filter=False)
+rows_affected = data.gene_id.isin(set(systematic_id_dict['systematic_id']))
+data.loc[rows_affected, 'gene_id'] = data.loc[rows_affected, 'gene_id'].map(dict(zip(systematic_id_dict['systematic_id'], systematic_id_dict['synonym'])))
+data = data[data['gene_id'] != '']
+
+# We make sure that no invalid systematic id is present
+valid_systematic_ids = pandas.read_csv('data/gene_IDs_names.tsv', sep='\t', names=['gene_systematic_id', 'gene_name', 'synonyms'], na_filter=False)
+
+valid_ids = data['gene_id'].isin(set(valid_systematic_ids['gene_systematic_id']))
+if not all(valid_ids):
+    raise ValueError('invalid systematic ids: {}'.format(set(data.loc[~valid_ids, 'gene_id'])))
+
 column_order = [
     'Gene systematic ID',
     'FYPO ID',
